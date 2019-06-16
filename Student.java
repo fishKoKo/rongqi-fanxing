90c769d52523fefa4cb8c150083069799ab08722
fishKoKo <49087865+fishKoKo@users.noreply.github.com>
2019-06-16 23:37:16 +0800

    1

---
+++ Student.java
@@ -0,0 +1,34 @@
+package rongqi_fanxing;
+
+public class Student {
+
+	private String num;
+	private String name;
+	private int point;
+	
+	public Student(String num,String name,int point) {
+		super();
+		this.num=num;
+		this.name=name;
+		this.point=point;
+	}
+	
+	public String getNum() {
+		return num;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getSum() {
+		return point;
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+
+	}
+
+}
